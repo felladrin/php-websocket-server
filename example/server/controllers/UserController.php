42c8a0eaf0d44b05363c42c318201819c075fa12
Victor Nogueira <felladrin@gmail.com>
2015-11-11 19:56:53 -0300

    Removed unused action.

--- example/server/controllers/UserController.php
+++ example/server/controllers/UserController.php
@@ -2,13 +2,6 @@
 
 class UserController
 {
-    public function actionRename()
-    {
-        $newName = WebSocketRequest::getParameter('name');
-        WebSocketRequest::$sender->set('name', $newName);
-        WebSocketRequest::broadcast('user', 'rename', array('name' => $newName));
-    }
-
     public function actionSetup()
     {
         WebSocketRequest::reply('user', 'load-user-list', User::getUserList());
